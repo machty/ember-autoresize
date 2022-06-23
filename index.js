f4f779fce92ae0a5aedde23b7982290fca1e1929
machty <machty@gmail.com>
2022-06-23 14:56:39 -0400

    fix missing var

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
 
     checker.for('ember-cli').assertAbove('2.15.0', '`ember-cli` 2.15 or greater is required to use this version of ember-autoresize. Use ember-autoresize@0.6.1 if you\'re using an older version of ember-cli.');
 
-    app.import(path.join(s, '../../amd/dom-ruler.js'), {
+    app.import(path.join(require.resolve('dom-ruler'), '../../amd/dom-ruler.js'), {
       exports: {
         'dom-ruler': ['default']
       }
