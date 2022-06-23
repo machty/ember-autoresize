cdcadc8632d07dddf8fa733151a7f3c463cac13c
machty <machty@gmail.com>
2022-06-23 14:53:52 -0400

    try require.resolve

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 'use strict';
 const VersionChecker = require('ember-cli-version-checker');
+const path = require('path');
 
 module.exports = {
   name: 'ember-autoresize',
@@ -11,7 +12,7 @@ module.exports = {
 
     checker.for('ember-cli').assertAbove('2.15.0', '`ember-cli` 2.15 or greater is required to use this version of ember-autoresize. Use ember-autoresize@0.6.1 if you\'re using an older version of ember-cli.');
 
-    app.import('node_modules/dom-ruler/dist/amd/dom-ruler.js', {
+    app.import(path.join(s, '../../amd/dom-ruler.js'), {
       exports: {
         'dom-ruler': ['default']
       }
